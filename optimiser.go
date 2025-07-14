0ea52b90742fc201e57dd7a2f07dd8eee99f0843
Richard Draycott <rich@wikoion.dev>
2025-07-14 18:44:43 +0100

    feat: Add linux-arm64 binary and x-platform build

--- optimiser.go
+++ optimiser.go
@@ -6,7 +6,7 @@ package optimiser
 /*
 #cgo darwin,arm64 LDFLAGS: -L${SRCDIR}/lib -Wl,-rpath,${SRCDIR}/lib -loptimiser_darwin_arm64
 #cgo linux,amd64 LDFLAGS: -L${SRCDIR}/lib -Wl,-rpath,${SRCDIR}/lib -loptimiser
-#cgo linux,arm64 LDFLAGS: -L${SRCDIR}/lib -Wl,-rpath,${SRCDIR}/lib -loptimiser_linux_arm6
+#cgo linux,arm64 LDFLAGS: -L${SRCDIR}/lib -Wl,-rpath,${SRCDIR}/lib -loptimiser_linux_arm64
 #include <dlfcn.h>
 #include <stdlib.h>
 
